40dc70c7a9011c52cdbd32a74a6f845310a61e7a
George Popoff <popoff96@live.com>
2018-03-18 17:13:37 +0300

    Добавил базовый класс для однофрагментных активностей

---
+++ app/src/main/java/com/breezesoftware/stayfit/core/SingleFragmentActivity.kt
@@ -0,0 +1,33 @@
+package com.breezesoftware.stayfit.core
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import com.breezesoftware.stayfit.R
+
+/**
+ * This file is part of Test Kotlin Application
+ *
+ * You can do everything with the code and files
+ *
+ * Created by popof on 18.03.2018.
+ */
+
+abstract class SingleFragmentActivity() : AppCompatActivity() {
+
+    protected abstract fun createFragment() : Fragment;
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.single_fragment_activity);
+
+        val fm = supportFragmentManager;
+        var fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
