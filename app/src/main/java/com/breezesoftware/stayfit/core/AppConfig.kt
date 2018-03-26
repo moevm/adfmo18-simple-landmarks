241e9306ce9a9ef7fcbd773418a70fd2d0b20317
George Popoff <popoff96@live.com>
2018-03-26 22:57:19 +0300

    Добавил класc конфигурации приложения

---
+++ app/src/main/java/com/breezesoftware/stayfit/core/AppConfig.kt
@@ -0,0 +1,13 @@
+package com.breezesoftware.stayfit.core
+
+/**
+ * This file is part of Test Kotlin Application
+ *
+ * You can do everything with the code and files
+ *
+ * Created by popof on 26.03.2018.
+ */
+
+class AppConfig {
+    val SERVER_URL = "http://localhost"
+}
